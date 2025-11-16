d59154b46f3da33fb0ab2e3e3e0c69d17274df01
p9co <ramazan01012005@gmail.com>
2025-11-17 00:06:24 +0300

    Создать хедер

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+// components/Header/Header.tsx
+import Link from 'next/link';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <nav className="header__nav">
+        <div className="header__left">
+          <Link href="/about" className="header__link">О проекте</Link>
+        </div>
+        
+        <div className="header__center">
+          <Link href="/" className="header__logo">arc</Link>
+        </div>
+        
+        <div className="header__right">
+          <Link href="/delivery" className="header__link">Доставка</Link>
+          <Link href="/contacts" className="header__link">Контакты</Link>
+          <button 
+            type="button" 
+            className="header__link" 
+          
+            style={{ background: 'none', border: 'none', cursor: 'pointer', padding: 0 }}
+          >
+            Корзина
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
