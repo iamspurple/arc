64798037ff649300094b17757cdfed03b2c57f92
p9co <ramazan01012005@gmail.com>
2025-11-27 15:59:04 +0300

    Добавить страницу контаков

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import './contacts.scss'
+import Link from 'next/link'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const Page = () => {
+	return (
+		<div className="contacts-page">
+			<h1 className="contacts-page__title">Контакты</h1>
+
+			<div className="contacts-page__content">
+				<p className="contacts-page__content-subtitle">
+					Если у вас возникнут вопросы или сложности с заказом, пожалуйста,
+					свяжитесь с нами.
+				</p>
+
+				<p>
+					E-mail: <Link href="mailto:arc@gmail.com">arc@gmail.com</Link>
+				</p>
+
+				<p>Telegram:</p>
+
+				<p>
+					Также вы можете связаться с нами в{' '}
+					<Link href="https://t.me/xxx">direct message</Link>
+				</p>
+			</div>
+		</div>
+	)
 }
 
-export default page
+export default Page
