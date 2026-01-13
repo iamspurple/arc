970ad42180d51d8fb0e0f135fe8bf40d5ff480d8
Sabina Gasanova <sabina21gasanova@yandex.ru>
2026-01-13 14:26:52 +0300

    декомпозировать страницу оформления заказа

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,99 +1,44 @@
 "use client";
 
-import Image from "next/image";
-
-import { useState } from "react";
-
 import { useCart } from "@/context/CartContext";
-
 import style from "./checkout.module.scss";
+import { formatPrice } from "@/services";
+import Form from "@/components/Checkout/Form/Form";
+import TotalList from "@/components/Checkout/TotalList/TotalList";
 
 const CheckoutPage = () => {
-	const { items, totalItems, totalPrice } = useCart();
-
-	const [formData, setFormData] = useState({
-		name: "",
-		surname: "",
-		email: "",
-		phone: "",
-		messenger: "",
-	});
+	const { items, totalPrice } = useCart();
 
 	if (items.length === 0) return <p>Для оформления заказа добавьте товары в корзину</p>;
 
 	return (
 		<div className={style.checkout_page}>
-			<h3>Оформление заказа</h3>
+			<h3>Оформление заявки на заказ</h3>
 
 			<div className={style.container}>
-				<form action="" className={style.form}>
-					<span></span>
-					<div className={style.form_field}>
-						<label htmlFor="name">Имя</label>
-						<input type="text" placeholder="Имя" />
+				<div>
+					<div className={style.wrapper}>
+						<span style={{ lineHeight: 1.6 }}>
+							1. Заполните форму, чтобы администратор смог связаться с вами для уточнения деталей
+							доставки
+						</span>
+						<Form />
 					</div>
-					<div className={style.form_field}>
-						<label htmlFor="surname">Фамилия</label>
-						<input type="text" placeholder="Фамилия" />
-					</div>
-
-					<div className={style.form_field}>
-						<label htmlFor="email">E-mail</label>
-						<input type="email" placeholder="E-mail" />
-					</div>
-					<div className={style.form_field}>
-						<label htmlFor="phone">Номер телефона</label>
-						<input type="tel" name="" id="" placeholder="Телефон" />
-					</div>
-
-					<div className={style.select_field}>
-						<label className={style.select_label} htmlFor="messenger">
-							Cпособ связи:
-						</label>
-						<select name="" id="">
-							<option value="">-</option>
-							<option value="email">Электронная почта</option>
-							<option value="telegram">Telegram</option>
-							<option value="whatsapp">Whatsapp</option>
-						</select>
-					</div>
-				</form>
-				<div className={style.total}>
-					<span>Ваш заказ:</span>
-
-					<ul>
-						{items.map((item) => (
-							<li key={`${item.id}-${item.size}`}>
-								<div className={style.item}>
-									{item.image && (
-										<div className={style.item_image}>
-											<Image
-												src={item.image}
-												alt={item.title}
-												fill
-												sizes="80px"
-												style={{ objectFit: "cover" }}
-											/>
-										</div>
-									)}
-
-									<div className={style.details}>
-										<span>
-											{item.title}, {item.size}
-										</span>
-										<span>{item.quantity}шт.</span>
-									</div>
-								</div>
-								<span>{item.price * item.quantity}</span>
-							</li>
-						))}
-					</ul>
-
-					<div>
-						<div>
-							<span>Итого</span>
-							<span>{totalPrice}</span>
+				</div>
+				<div>
+					<div className={style.wrapper}>
+						<span style={{ lineHeight: 1.6 }}>
+							2. Проверьте добавленные в корзину позиции и отправьте заявку
+						</span>
+						<TotalList items={items} />
+						<div className={style.total}>
+							<span>Итого:</span>
+							<span>{formatPrice(totalPrice)}</span>
 						</div>
+
+						<button form="checkoutForm" type="submit" className={style.button}>
+							Отправить
+						</button>
 					</div>
 				</div>
 			</div>
