5995315e881b869f718adde47e8cf28b3dd338e2
Sabina Gasanova <sabina21gasanova@yandex.ru>
2026-02-01 17:24:51 +0300

    create basic layout for order page

--- src/app/(auth)/dashboard/order/page.tsx
+++ src/app/(auth)/dashboard/order/page.tsx
@@ -1,7 +1,54 @@
+import type { TableColumnsType } from "antd";
+
+import { DataTable } from "@/components/Dashboard/DataTable/DataTable";
+import { Header } from "@/components/Dashboard/Header";
+
 export default function Order() {
+	const columns: TableColumnsType = [
+		{
+			title: "ID",
+			dataIndex: "id",
+		},
+
+		{
+			title: "Статус",
+			dataIndex: "status",
+		},
+		{
+			title: "Дата",
+			dataIndex: "date",
+		},
+
+		{
+			title: "Сумма",
+			dataIndex: "amount",
+		},
+
+		{
+			title: "Заказчик",
+			dataIndex: "customer",
+		},
+
+		{
+			title: "Номер телефона",
+			dataIndex: "phone",
+		},
+
+		{
+			title: "E-mail",
+			dataIndex: "email",
+		},
+
+		{
+			title: "",
+			dataIndex: "actions",
+		},
+	];
+
 	return (
 		<>
-			<h3>Страница с заказами</h3>
+			<Header />
+			<DataTable columns={columns} />
 		</>
 	);
 }
