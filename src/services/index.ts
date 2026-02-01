ebd5297ab023d706ff784449a220bf7342b4abd3
Sabina Gasanova <sabina21gasanova@yandex.ru>
2026-02-01 17:34:59 +0300

    createOrder func

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,25 @@
 export const formatPrice = (price: number) => {
 	return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 };
+
+export const createOrder = (size: string): number => {
+	const str = size.toLowerCase();
+
+	switch (str) {
+		case "xs":
+			return 0;
+			break;
+		case "s":
+			return 1;
+			break;
+		case "m":
+			return 2;
+			break;
+		case "l":
+			return 3;
+			break;
+		default:
+			return 1;
+			break;
+	}
+};
