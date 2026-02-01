809823c442fe10ecdfa2b7f345dfb47689a6b364
Sabina Gasanova <sabina21gasanova@yandex.ru>
2026-02-01 17:21:01 +0300

    create tanstack provider and queries

---
+++ src/entities/product/api/useProductsQuery.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import type { Product, ProductSize } from "@prisma/client";
+import { getProducts, getProductSizes } from "../server";
+
+import type { ProductOption } from "@prisma/client";
+import { getProductOptionById, getProductOptions } from "../server";
+
+export const PRODUCTS_QUERY_KEY = ["products"];
+export const PRODUCT_OPTIONS_QUERY_KEY = ["product-options"];
+
+export const useProductsQuery = () => {
+	return useQuery<Product[]>({
+		queryKey: PRODUCTS_QUERY_KEY,
+		queryFn: () => getProducts(),
+		staleTime: 60_000,
+	});
+};
+
+export const useProductOptionsQuery = () => {
+	return useQuery<ProductOption[]>({
+		queryKey: [PRODUCT_OPTIONS_QUERY_KEY],
+		queryFn: () => getProductOptions(),
+		staleTime: 60_000,
+	});
+};
+
+export const useProductOptionByIdQuery = (id: string) => {
+	return useQuery<ProductOption | null>({
+		queryKey: [PRODUCT_OPTIONS_QUERY_KEY, id],
+		queryFn: () => getProductOptionById(id),
+		staleTime: 60_000,
+	});
+};
+
+export const useProductSizesQuery = (id: string) => {
+	return useQuery<ProductSize[] | null>({
+		queryKey: ["product-size", id],
+		queryFn: () => getProductSizes(),
+		staleTime: 60_000,
+	});
+};
